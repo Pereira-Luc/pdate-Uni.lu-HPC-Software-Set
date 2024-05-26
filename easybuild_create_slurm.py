96caff11eeca2e137b75345959e054767e38360a
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2024-05-26 13:27:25 +0200

    wokring slurm creator

--- easybuild_create_slurm.py
+++ easybuild_create_slurm.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 # Select "foss" to install foss toolchain 2023a
 # Select "intel" to install foss toolchain 2023a
 toolchain = "foss"
@@ -16,84 +13,70 @@ max_walltime = "5"
 dry_run = False
 
 
-def generate_slurm_script(eb_files: List[str],
-                          script_filename: str = "install_modules.sh") -> None:
+def generate_slurm_script(eb_files, script_filename="test_install_modules.sh", dry_run=False):
     """
     Generates a Slurm script to load and install modules using EasyBuild with specified eb files,
     with an option for a dry run to simulate installations using GNU Parallel.
 
     Args:
-        eb_files List[str]: List of full paths to EasyBuild (.eb) files.
+        eb_files (list): List of full paths to EasyBuild (.eb) files.
         script_filename (str): Filename for the Slurm script.
+        dry_run (bool): Whether to perform a dry run instead of a real installation.
     """
-    # Check if the list of EasyBuild files is empty
     if not eb_files:
         print("No EasyBuild files provided. Exiting...")
         return
 
-    # Additional check if the list length is zero
     if len(eb_files) == 0:
         print("Error: No modules to install. Please check your file list.")
         return
 
-    # Write the slurm script
     with open(script_filename, "w") as file:
         # Write the Slurm script header
         file.write("#!/bin/bash -l\n")
-        file.write(f"#SBATCH --job-name=install_eb_modules_{toolchain}\n")
-        file.write(f"#SBATCH --output=install_eb_modules_{toolchain}_%j.out\n")
-        file.write("#SBATCH --time=02:00:00\n")
+        file.write("#SBATCH --job-name=test-install-eb-modules\n")
+        file.write("#SBATCH --output=out/test-install-eb-module-%j.out\n")
+        file.write("#SBATCH --time=20:00:00\n")
         file.write("#SBATCH --partition=batch\n")
+        file.write("#SBATCH --nodes=1\n")
         file.write("#SBATCH --mem=0\n")
-
-        file.write("\n")
-
-        # Purge old modules and export system variable
-        file.write("# Purge old modules and export system variable\n")
-        file.write("module purge\n")
-        file.write("export EASYBUILD_JOB_BACKEND=Slurm\n")
         file.write("\n")
 
         # Function to print an error message and exit
         file.write("# Function to print an error message and exit\n")
-        file.write("print_error_and_exit() { echo \"***ERROR*** $*\"; exit 1; }\n")
+        file.write("module purge\n")
+        file.write("export EASYBUILD_JOB_BACKEND=Slurm\n")
         file.write("\n")
-
+        
+        file.write("print_error_and_exit() { echo \"***ERROR*** $*\"; exit 1; }\n")
+        
         # Check if GNU Parallel is installed
         file.write("# Check if GNU Parallel is installed\n")
         file.write("hash parallel 2>/dev/null && test $? -eq 0 || print_error_and_exit \"Parallel is not installed on the system\"\n")
         file.write("\n")
-
-        # Load the EasyBuild module
+        
+         # Load the EasyBuild module
         file.write("# Load the EasyBuild module\n")
         file.write("module load tools/EasyBuild/4.9.1\n")
         file.write("\n")
-
-        # Create an array of EasyBuild files
-        file.write("# Create an array of EasyBuild files\n")
+        
         file.write("EBFILES=(" + ' '.join(f'"{file}"' for file in eb_files) + ")\n")
+        file.write("mkdir -p logs\n")
         file.write("\n")
 
-        # Create a directory for logs
-        file.write("# Create a directory for logs\n")
-        file.write(f"mkdir -p eb_logs_{toolchain}\n")
-        file.write("\n")
-
-        # Set the command for EasyBuild
         command = "eb {} --robot --job" if not dry_run else "eb {} --robot -D"
+
         srun_command = "srun -n1 "
         verbose_flag = "--verbose"
 
-        # Use GNU Parallel to run EasyBuild installations
-        file.write("# Use GNU Parallel to run EasyBuild installations\n")
-        file.write(f"parallel -j $SLURM_CPUS_PER_TASK {verbose_flag} --joblog eb_logs_{toolchain}/eb_joblog.log ")
-        file.write(f"\"{srun_command} -c {job_cores} {command} --job-cores={job_cores} --job-max-walltime={max_walltime} --job-backend-config=slurm --trace --accept-eula-for=CUDA > logs/eb-log-{{#}}.log\" ::: \"${{EBFILES[@]}}\"\n")
-        file.write("\necho 'Tasks are all running. Use sq to see them.'\n")
+        file.write(f"parallel -j 16 {verbose_flag} --joblog eb-joblog.log ")
+        file.write(f"\"{srun_command} -c {job_cores} {command} --job-cores={job_cores} --job-max-walltime={max_walltime} --job-backend-config=slurm --trace --accept-eula-for=all > logs/eb-log-{{#}}.log\" ::: \"${{EBFILES[@]}}\"\n")
+        file.write("\necho 'Tasks are all runing now sq to see them'\n")
 
     print(f"Slurm script generated: {script_filename}")
 
 
-def read_eb_paths(file_path: str) -> List[str]:
+def read_eb_paths(file_path):
     """
     Reads EasyBuild configuration file paths from a text file.
 
@@ -101,17 +84,12 @@ def read_eb_paths(file_path: str) -> List[str]:
         file_path (str): Path to the text file containing EasyBuild file paths.
 
     Returns:
-        List[str]: A list of paths as strings.
+        list: A list of paths as strings.
     """
-    # Open the file in read mode
     with open(file_path, 'r') as file:
-        # Read each line, strip any surrounding whitespace, and filter out any empty lines
         paths = [line.strip() for line in file if line.strip()]
-
-    # Return the list of file paths
     return paths
-
-
+    
 if __name__ == "__main__":
     # Print a message indicating the start of the Slurm script creation process
     print("Creating associated Slurm script...")
@@ -124,4 +102,4 @@ if __name__ == "__main__":
     eb_files = read_eb_paths(input_file)
 
     # Generate the Slurm script with the read EasyBuild files
-    generate_slurm_script(eb_files, script_file)
+    generate_slurm_script(eb_files, script_file, dry_run=dry_run)
